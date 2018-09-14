ca686f21378ec18e4cb2bc9bc68d51ae69cbd74f
Harry Zhang <hzhang@ea.com>
2018-09-14 16:48:03 -0700

    fix build path

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use('/api/posts', posts);
 if(process.env.NODE_ENV === "production"){
 	app.use(express.static('/client/static'));
 	app.get('*', (request, response)=>{
-		response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		response.sendFile(path.resolve(__dirname, 'static-client', 'build', 'index.html'));
 	});
 }
 
