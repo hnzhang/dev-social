eb3a29d71b0857bb514414575e26115f7672605a
Harry Zhang <hnzhang@gmail.com>
2018-09-15 14:48:18 -0700

    add private router to hide content

--- static-client/src/App.js
+++ static-client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {setCurrentUser, logoutUser} from './actions/loginAction';
 import {clearCurrentProfile} from './actions/profileAction';
 import setAuthToken from './utils/setAuthToken';
@@ -7,6 +7,8 @@ import jwtDecode from 'jwt-decode';
 import {Provider}  from 'react-redux';
 
 import store from './store';
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -45,7 +47,9 @@ class App extends Component {
             <div className="container"> 
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
-              <Route exact path='/dashboard' component={Dashboard} />
+              <Switch>
+                <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              </Switch>
               <Route exact path='/create-profile' component={CreateProfile} />
               <Route exact path='/edit-profile' component={EditProfile} />
             </div>
--- static-client/src/components/common/PrivateRoute.js
+++ static-client/src/components/common/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-//import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 const PrivateRoute = ({ component: Component, auth, ...rest }) => (
@@ -23,5 +23,5 @@ PrivateRoute.propTypes = {
 const mapStateToProps = state => ({
   auth: state.auth
 });
-export default PrivateRoute;
-//export default connect(mapStateToProps)(PrivateRoute);
+
+export default connect(mapStateToProps)(PrivateRoute);
