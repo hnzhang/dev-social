85d6ced6d452e4bf753c0de0c190599e79654649
Harry Zhang <hzhang@ea.com>
2018-09-15 07:43:13 -0700

    fix authentication

--- static-client/src/actions/authAction.js
+++ static-client/src/actions/authAction.js
@@ -1,9 +1,15 @@
 //register user
-import {TEST_DISPATCH} from './types';
-export const registerUser = (userData) =>{
+import axios from 'axios';
 
-	return {
-		type: TEST_DISPATCH,
-		payload: userData,
-	}
+import { GET_ERRORS} from './types';
+export const registerUser = (userData, history) => dispatch=>{
+    axios.post('/api/users/register', userData)
+      .then(response=>{
+				history.push('/login');
+				//console.log(response.data);
+			})
+      .catch(err=>{
+				//console.log("get errors", GET_ERRORS);
+					dispatch({ type: GET_ERRORS, payload: err.response.data, });
+      });
 }
